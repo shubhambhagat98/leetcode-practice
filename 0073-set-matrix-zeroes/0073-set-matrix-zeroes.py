c3aa39073d64c61144dd0006d67fcb396abee965
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2022-11-19 21:22:51 -0500

    Time: 124 ms (98.86%), Space: 14.8 MB (53.83%) - LeetHub

--- 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
+++ 0073-set-matrix-zeroes/0073-set-matrix-zeroes.py
@@ -3,27 +3,36 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows, col = set(), set()
+        
+        ROWS = len(matrix)
+        COLS = len(matrix[0])
+        
+        rowZeroFlag = False
         
         
-        for r in range(len(matrix)):
-            for c in range(len(matrix[0])):
+        for r in range(ROWS):
+            for c in range(COLS):
                 if matrix[r][c] == 0:
-                    rows.add(r)
-                    col.add(c)
+                    matrix[0][c] = 0
                     
-        
-        print(f"rows ==>  {rows}")
-        print(f"col ==> {col}")
-        
-        
-        for r in rows:
-            for c in range(len(matrix[0])):
-                if matrix[r][c] != 0:
+                    if r > 0:
+                        matrix[r][0] = 0
+                    else:
+                        rowZeroFlag = True
+                    
+                    
+        for r in range(1,ROWS):
+            for c in range(1, COLS):
+                if matrix[r][0] == 0 or matrix[0][c] == 0:
                     matrix[r][c] = 0
         
-        for c in col:
-            for r in range(len(matrix)):
-                if matrix[r][c] != 0:
-                    matrix[r][c] = 0
+        if matrix[0][0] == 0:
+            for r in range(1, ROWS):
+                matrix[r][0] = 0
+        
+        if rowZeroFlag == True:
+            for c in range(0, COLS):
+                matrix[0][c] = 0
+                
+                
                     
