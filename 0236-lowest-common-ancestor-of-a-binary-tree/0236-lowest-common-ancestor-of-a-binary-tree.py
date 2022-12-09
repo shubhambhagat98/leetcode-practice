fc7b07f27641f071343b8b419a0e057cf6c55528
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2022-12-09 01:20:56 -0500

    Time: 83 ms (82.80%), Space: 31.2 MB (5.09%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -17,26 +17,29 @@ class Solution:
             return set()
         
         
-        left_set = self.getLCA(root.left, p, q, depth+1)
-        right_set = self.getLCA(root.right, p, q, depth +1)
+        union_set = set()
         
-        union_set = left_set | right_set
-        
-        if not union_set:
-            if root.val == p.val or root.val == q.val: 
-                return {root.val}
-            else:
-                return set()
-        elif len(union_set) == 2:
-            if depth > self.lowest_ancestor[1] :
-                print(f"curr LCA ==> {root.val} | depth => {depth}")
-                self.lowest_ancestor = (root, depth)
-        elif len(union_set) == 1:
-            if (root.val == p.val or root.val == q.val) and (root.val not in union_set):
-                union_set.add(root.val)
+        if not self.lowest_ancestor[0]:
+            left_set = self.getLCA(root.left, p, q, depth+1)
+            right_set = self.getLCA(root.right, p, q, depth +1)
+
+            union_set = left_set | right_set
+
+            if not union_set:
+                if root.val == p.val or root.val == q.val: 
+                    return {root.val}
+                else:
+                    return set()
+            elif len(union_set) == 2:
                 if depth > self.lowest_ancestor[1] :
                     print(f"curr LCA ==> {root.val} | depth => {depth}")
-                    self.lowest_ancestor = (root, depth)       
+                    self.lowest_ancestor = (root, depth)
+            elif len(union_set) == 1:
+                if (root.val == p.val or root.val == q.val) and (root.val not in union_set):
+                    union_set.add(root.val)
+                    if depth > self.lowest_ancestor[1] :
+                        print(f"curr LCA ==> {root.val} | depth => {depth}")
+                        self.lowest_ancestor = (root, depth)       
         return union_set
             
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
