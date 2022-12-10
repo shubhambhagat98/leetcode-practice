9f16490945434091bb29c3c5fc7dde961bca8f98
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2022-12-10 14:40:29 -0500

    Time: 984 ms (14.66%), Space: 18 MB (42.61%) - LeetHub

--- 0286-walls-and-gates/0286-walls-and-gates.py
+++ 0286-walls-and-gates/0286-walls-and-gates.py
@@ -1,34 +1,35 @@
 class Solution:
     
+    empty = 2147483647
     
-    def isValid(self, row, col, m, n, rooms, dist):
+    
+    def isValid(self, row, col, m, n, rooms):
         
-        if 0 <= row < m and 0<= col < n and rooms[row][col] > dist + 1:
+        if 0 <= row < m and 0<= col < n and rooms[row][col] == self.empty:
             return True
         
         return False
     
-    def successors(self, row, col, rooms, dist):
+    def successors(self, row, col, rooms):
         successors = [(row+1, col), (row-1,col), (row, col+1), (row, col-1)]
         
-        return [succ for succ in successors if self.isValid(succ[0], succ[1], len(rooms), len(rooms[0]), rooms, dist)    ]
+        return [succ for succ in successors if self.isValid(succ[0], succ[1], len(rooms), len(rooms[0]), rooms)    ]
     
     
     
-    def bfs(self, start_row, start_col, rooms):
+    def bfs(self, gates, rooms):
         
-        queue = [((start_row, start_col), 0)]
+        queue = gates
         
-        visited = set(((start_row, start_col), 0))
+        # visited = set(((start_row, start_col), 0))
         
         while queue:
             current, dist = queue.pop(0)
-            for succ in self.successors(*current, rooms, dist):
-                if succ not in visited:
-                    queue.append((succ, dist + 1))
-                    # if rooms[succ[0]][succ[1]] > dist + 1:
-                    rooms[succ[0]][succ[1]] = dist + 1
-                    visited.add(succ)
+            for succ in self.successors(*current, rooms):
+                queue.append((succ, dist + 1))
+                # if rooms[succ[0]][succ[1]] > dist + 1:
+                rooms[succ[0]][succ[1]] = dist + 1
+                # visited.add(succ)
                     
             
     
@@ -37,8 +38,20 @@ class Solution:
         """
         Do not return anything, modify rooms in-place instead.
         """
+        
+        gates = []
+        
         for row in range(len(rooms)):
             for col in range(len(rooms[0])):
                 if rooms[row][col] == 0:
-                    self.bfs(row, col, rooms)
+                    gates.append(((row,col), 0))
+        
+        self.bfs(gates, rooms)
+                    
+                    
+                    
+                    
+                    
+                    
+                    
         
