473bff27e46bdf9231dd92e7da92f6facd7aeaef
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2022-12-13 21:05:53 -0500

    Time: 403 ms (79.69%), Space: 16.5 MB (66.21%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.py
+++ 0200-number-of-islands/0200-number-of-islands.py
@@ -18,29 +18,36 @@ class Solution:
     
     
     
-    def bfs(self, start_row, start_col, grid):
-        
-        queue = [(start_row, start_col)]
-        self.visited.add((start_row, start_col))
-        current_island = [(start_row, start_col)]
-        
-        while queue:
-            curr_row, curr_col = queue.pop(0)
-            
-            for succ in self.successors(curr_row, curr_col, grid):
-                if succ not in self.visited:
-                    queue.append(succ)
-                    self.visited.add(succ)
-                    current_island.append(succ)
-        print(f"current Island ==> {current_island}")
+    # def bfs(self, start_row, start_col, grid):   
+    #     queue = [(start_row, start_col)]
+    #     self.visited.add((start_row, start_col))
+    #     current_island = [(start_row, start_col)]
+    #     while queue:
+    #         curr_row, curr_col = queue.pop(0)    
+    #         for succ in self.successors(curr_row, curr_col, grid):
+    #             if succ not in self.visited:
+    #                 queue.append(succ)
+    #                 self.visited.add(succ)
+    #                 current_island.append(succ)
+    #     print(f"current Island ==> {current_island}")
                 
+    def dfs(self, row, col, grid):
+        # print(f"\ncurrent : {row}, {col} ")
+        for (succ_row, succ_col) in self.successors(row, col, grid):
+            # print(succ_row, succ_col, end= " | ")
+            grid[succ_row][succ_col] = "0"
+            self.dfs(succ_row, succ_col, grid)
+            
+            
+            
+    
     def numIslands(self, grid: List[List[str]]) -> int:
         
         for row in range(len(grid)):
             for col in range(len(grid[0])):
-                if grid[row][col] == "1" and (row, col) not in self.visited:
+                if grid[row][col] == "1":
                     self.count += 1
-                    self.bfs(row, col, grid)
+                    self.dfs(row, col, grid)
                     
-        
+        # print("\n",grid)
         return self.count 
