be42c5058dfa56779768e5519597c1787462370a
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2022-12-16 17:30:32 -0500

    Time: 30 ms (95.29%), Space: 13.9 MB (63.93%) - LeetHub

--- 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
+++ 0119-pascals-triangle-ii/0119-pascals-triangle-ii.py
@@ -7,17 +7,20 @@ class Solution:
         if current_index - 1 == rowIndex:
             return current_row
         
-        new_row = [1 for x in range(current_index+1)]
+        new_row = [1]
         
-        if len(new_row) > 2:
-            for i in range(1, len(new_row)-1):
-                new_row[i] = current_row[i-1]+current_row[i]
+        if (current_index+1) > 2:
+            for i in range(1, current_index):
+                new_row.append(current_row[i-1]+current_row[i])
         # print(f"new row: {new_row}\n")
+        new_row.append(1)
         return self.getRowRec(new_row, current_index+1, rowIndex)
         
         
     
     def getRow(self, rowIndex: int) -> List[int]:
+        
+        
         
         
         
