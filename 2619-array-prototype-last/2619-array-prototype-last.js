862bc6b2c3a086aeaddc4f72f9bb1f869100cdf2
Shubham Bhagat <53030762+shubhambhagat98@users.noreply.github.com>
2023-04-17 14:20:15 -0400

    Time: 60 ms (37.55%), Space: 42.1 MB (39.77%) - LeetHub

--- 2619-array-prototype-last/2619-array-prototype-last.js
+++ 2619-array-prototype-last/2619-array-prototype-last.js
@@ -3,9 +3,7 @@ Array.prototype.last = function() {
     console.log(this);
     if(this.length > 0){
         
-        for( var i =0; i<this.length; i++){
-            if (i == this.length -1) return this[i]
-        } 
+        return this[this.length -1];
     }
     
     return -1;
